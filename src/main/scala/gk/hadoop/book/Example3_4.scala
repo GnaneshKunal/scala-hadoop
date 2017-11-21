4725d7bb04a3d535da1ae60fc47f246d7e4a67b3
Gnanesh <gnaneshkunal@outlook.com>
2017-11-21 09:35:33 +0530

    Copy a local file to Hadoop fs

---
+++ src/main/scala/gk/hadoop/book/Example3_4.scala
@@ -0,0 +1,30 @@
+package gk.hadoop.book
+
+import org.apache.hadoop.fs._
+import org.apache.hadoop.io._
+import org.apache.hadoop.conf._
+import java.io._
+import java.net.URI
+
+import org.apache.hadoop.util.Progressable
+
+// Copy a local file to Hadoop fs
+object Example3_4 {
+
+  def main(args: Array[String]): Unit = {
+    val localStr: String = "/home/ub/spark/README.md"
+    val strDestination: String = "hdfs://localhost:9000/data/test3.txt"
+
+    val in = new BufferedInputStream(new FileInputStream(localStr))
+
+    val conf = new Configuration()
+    val fs = FileSystem.get(URI.create(strDestination), conf)
+    val out = fs.create(new Path(strDestination), new Progressable() {
+      override def progress(): Unit = {
+        print(".")
+      }
+    })
+
+    IOUtils.copyBytes(in, out, 4096, true)
+  }
+}
