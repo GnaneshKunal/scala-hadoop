bcbf8f57b2adf815e0e02a6eee84099e6ff6653d
Gnanesh <gnaneshkunal@outlook.com>
2017-11-30 17:27:29 +0530

    add spark maxtemp

---
+++ src/main/scala/gk/hadoop/book/scala/MaxTemp.scala
@@ -0,0 +1,23 @@
+package gk.hadoop.book.scala
+
+import org.apache.spark.sql._
+
+object MaxTemp extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Max Temp")
+    .master("local[*]")
+    .getOrCreate()
+
+  val sc = spark.sparkContext
+
+  sc.textFile("hdfs://localhost:9000/data/temp.txt")
+    .map(_.split('\t'))
+    .filter(rec => rec(1) != "9999" && rec(2).matches("[01459]"))
+    .map(rec => (rec(0).toInt, rec(1).toInt))
+    .reduceByKey(Math.max)
+    .saveAsTextFile("hdfs://localhost:9000/tmp/scmaxtemp")
+
+  spark.stop()
+
+}
