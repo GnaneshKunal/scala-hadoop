febcf48330a5546a2a8812f2cb370c4fbbe03d55
Gnanesh <gnaneshkunal@outlook.com>
2017-12-01 16:14:08 +0530

    scala -> spark

---
+++ src/main/scala/gk/hadoop/book/spark/SparkInit.scala
@@ -0,0 +1,28 @@
+package gk.hadoop.book.scala
+
+import org.apache.spark.sql._
+
+object SparkInit extends App {
+
+  val spark = SparkSession
+    .builder()
+    .appName("SparkInit")
+    .master("local[*]")
+    .getOrCreate()
+
+  spark.conf
+    .set("spark.serializer",
+    "org.apache.spark.serializer.KyroSerializer")
+
+  val sc = spark.sparkContext
+
+  val range = 0 to 10
+
+  val params = sc.parallelize(range, 10)
+
+  val files = sc.wholeTextFiles("/home/ub/spark/README.md")
+  files.collect()(0)._1
+
+  spark.stop()
+
+}
